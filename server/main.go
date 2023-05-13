cf986464b9de02590739f1b62d2bea4f76348718
iwanhae <iwanhae@gmail.com>
2023-05-13 11:05:24 +0900

    feat: cpu and memory endpoint

--- server/main.go
+++ server/main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"golang.org/x/time/rate"
 )
+
+var dummyMemory = []byte{}
 
 func main() {
 	hostname, err := os.Hostname()
@@ -49,15 +54,97 @@ func main() {
 		}
 		req := c.Request()
 		c.Response().Header().Add("color", *color)
-		return c.JSON(http.StatusOK, map[string]interface{}{
+		return c.JSONPretty(http.StatusOK, map[string]interface{}{
 			"hostname":    hostname,
 			"host":        req.Host,
 			"method":      req.Method,
 			"url":         req.URL.String(),
 			"headers":     req.Header,
 			"remote_addr": req.RemoteAddr,
-		})
+		}, "  ")
+	})
+
+	e.GET("/cpu/:length", func(c echo.Context) error {
+		length, err := strconv.Atoi(c.Param("length"))
+		if err != nil {
+			return err
+		}
+
+		now := time.Now()
+		pi := PiMultiThread(8, length)
+
+		c.Response().Header().Add("color", *color)
+		c.Response().Header().Add("color", *color)
+		return c.JSONPretty(http.StatusOK, map[string]interface{}{
+			"takes": time.Since(now).Milliseconds(),
+			"pi":    fmt.Sprintf("%v", pi),
+		}, "  ")
+	})
+
+	e.GET("/memory/:length", func(c echo.Context) error {
+		length, err := strconv.Atoi(c.Param("length"))
+		if err != nil {
+			return err
+		}
+
+		now := time.Now()
+		dummyMemory = make([]byte, length*1024*1024*1024)
+		for i := range dummyMemory {
+			dummyMemory[i] = '1'
+		}
+
+		c.Response().Header().Add("color", *color)
+		return c.JSONPretty(http.StatusOK, map[string]interface{}{
+			"takes":    time.Since(now).Milliseconds(),
+			"length":   len(dummyMemory),
+			"lengthKB": len(dummyMemory) / 1024,
+			"lengthMB": len(dummyMemory) / 1024 / 1024,
+			"lengthGB": len(dummyMemory) / 1024 / 1024 / 1024,
+		}, "  ")
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+type Float64 struct {
+	value float64
+	lock  sync.RWMutex
+}
+
+func (f *Float64) Inc(diff float64) float64 {
+	f.lock.Lock()
+	ret := f.value + diff
+	f.value = ret
+	f.lock.Unlock()
+	return ret
+}
+
+func (f *Float64) Get() float64 {
+	f.lock.RLock()
+	ret := f.value
+	f.lock.RUnlock()
+	return ret
+}
+func partialSum(kStart int, kOffset int, amount int) (sum float64) {
+	for k := float64(kStart); k < float64(kStart+kOffset*amount); k += float64(kOffset) {
+		sum += 1 / math.Pow(16, k) * (4/(8*k+1) - 2/(8*k+4) - 1/(8*k+5) - 1/(8*k+6))
+	}
+
+	return
+}
+
+func PiMultiThread(workers int, iteration int) float64 {
+	wg := sync.WaitGroup{}
+	ret := Float64{}
+
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go (func(kStart int) {
+			ret.Inc(partialSum(int(kStart), workers, iteration))
+			wg.Done()
+		})(i)
+	}
+
+	wg.Wait()
+	return ret.Get()
 }
